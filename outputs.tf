4dbc6318b3bd1dbc36355cb150afe188920abd89
Liam F. O'Neill <liamfoneill@outlook.com>
2018-05-21 17:03:00 +0100

    Commented out unsupported outputs

--- outputs.tf
+++ outputs.tf
@@ -18,30 +18,30 @@ output "storageaccount_primary_blob_endpoint" {
   description = "The endpoint URL for blob storage in the primary location."
 }
 
-output "storageaccount_secondary_blob_endpoint" {
+/* output "storageaccount_secondary_blob_endpoint" {
   value       = "${azurerm_storage_account.storage_account.secondary_blob_endpoint}"
   description = "The endpoint URL for blob storage in the secondary location."
-}
+} */
 
 output "storageaccount_primary_queue_endpoint" {
   value       = "${azurerm_storage_account.storage_account.primary_queue_endpoint}"
   description = "The endpoint URL for queue storage in the primary location. "
 }
 
-output "storageaccount_secondary_queue_endpoint" {
+/* output "storageaccount_secondary_queue_endpoint" {
   value       = "${azurerm_storage_account.storage_account.secondary_queue_endpoint}"
   description = "The endpoint URL for queue storage in the secondary location."
-}
+} */
 
 output "storageaccount_primary_table_endpoint" {
   value       = "${azurerm_storage_account.storage_account.primary_table_endpoint}"
   description = "The endpoint URL for table storage in the primary location. "
 }
 
-output "storageaccount_secondary_table_endpoint" {
+/* output "storageaccount_secondary_table_endpoint" {
   value       = "${azurerm_storage_account.storage_account.secondary_table_endpoint}"
   description = "The endpoint URL for table storage in the secpondary location."
-}
+} */
 
 output "storageaccount_primary_file_endpoint" {
   value       = "${azurerm_storage_account.storage_account.primary_file_endpoint}"
@@ -78,8 +78,9 @@ output "storageaccount_primary_blob_connection_string" {
   sensitive   = true
 }
 
-output "storageaccount_secondary_blob_connection_string" {
+/* output "storageaccount_secondary_blob_connection_string" {
   value       = "${azurerm_storage_account.storage_account.secondary_blob_connection_string}"
   description = "The connection string associated with the secondary blob location."
   sensitive   = true
-}
+} */
+
