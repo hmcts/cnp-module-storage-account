1b2030347a5b44a8dd06709bef0115d9808e1726
timwebster9 <tim.webster@gmail.com>
2018-07-13 14:45:36 +0100

    create test resource group

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Terraservice Module for creating an Azure Resource Manager based storage account
 
 ## Testing
 The directories under `test/` provide different testing scenarios.  To use:
-* Make sure the resource group `cnp-devtest` exists in your subscription
-* Change to the directory of your choosing and run `terraform init`, followed by `terraform plan`, etc...
+* Change to the directory of your choosing and run `terraform init`
+* Run `terraform plan, apply`, etc...
--- test/with-random-string/module.tf
+++ test/with-random-string/module.tf
@@ -1,8 +1,13 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+}
+
 module "storage_account" {
   source                      = "../../"
   env                         = "${var.env}"
   storage_account_name        = "${var.storage_account_name}"
-  resource_group_name         = "${var.resource_group_name}"
+  resource_group_name         = "${azurerm_resource_group.rg.name}"
   location                    = "${var.location}"
   account_kind                = "${var.account_kind}"
   account_tier                = "${var.account_tier}"
