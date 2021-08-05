d3c4f6af3d4830924e638ac687cb64ca730e3c4a
Ciarán Carragher <c.carragher@kainos.com>
2021-08-05 11:48:00 +0100

    Add back dynamic block

--- main.tf
+++ main.tf
@@ -29,10 +29,10 @@ resource "azurerm_storage_account" "storage_account" {
   access_tier               = var.access_tier
   enable_https_traffic_only = var.enable_https_traffic_only
 
-  #dynamic "blob_properties" {
-  blob_properties {
-    #for_each = var.enable_data_protection == true ? [1] : []
-    #content {
+  dynamic "blob_properties" {
+  #blob_properties {
+    for_each = var.enable_data_protection == true ? [1] : []
+    content {
       versioning_enabled = true
       container_delete_retention_policy {
         days = 7
@@ -40,7 +40,7 @@ resource "azurerm_storage_account" "storage_account" {
       delete_retention_policy {
         days = 365
       }
-    #}
+    }
   }
 
   network_rules {
