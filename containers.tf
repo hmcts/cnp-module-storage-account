88f258d2339117a8ad7bf2c819c842ff40c53a15
Christopher Pateman <christopher.pateman@version1.com>
2021-08-11 14:13:35 +0100

    Apply comments

---
+++ containers.tf
@@ -0,0 +1,8 @@
+
+resource "azurerm_storage_container" "container" {
+  for_each = var.containers
+
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  name                  = each.value.name
+  container_access_type = each.value.access_type
+}
