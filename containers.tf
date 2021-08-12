201e4c2d7d1734c5eb1864406c5432464cb55a59
Christopher Pateman <christopher.pateman@version1.com>
2021-08-12 13:47:53 +0100

    correct for_each

--- containers.tf
+++ containers.tf
@@ -1,10 +1,6 @@
 
 resource "azurerm_storage_container" "container" {
-  for_each = [for container in var.containers : {
-    name        = container.name
-    access_type = container.access_type
-    resource    = "container"
-  }]
+  for_each = {for container in var.containers:  container.name => container}
   storage_account_name  = azurerm_storage_account.storage_account.name
   name                  = each.value.name
   container_access_type = each.value.access_type
