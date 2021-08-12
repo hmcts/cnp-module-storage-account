a48d19daa99a516eb8b1c24b5cd7532163b8766e
Christopher Pateman <christopher.pateman@version1.com>
2021-08-12 13:36:07 +0100

    Convert object to map

--- containers.tf
+++ containers.tf
@@ -1,7 +1,9 @@
 
 resource "azurerm_storage_container" "container" {
-  for_each = toset(var.containers)
-
+  for_each = [for container in var.containers : {
+    name        = container.name
+    access_type = container.access_type
+  }]
   storage_account_name  = azurerm_storage_account.storage_account.name
   name                  = each.value.name
   container_access_type = each.value.access_type
