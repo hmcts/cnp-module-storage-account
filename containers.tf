db18c4ab958187c3e2fa90437340565ef74b92a4
Christopher Pateman <christopher.pateman@version1.com>
2021-08-12 13:41:26 +0100

    Check fix tuple error

--- containers.tf
+++ containers.tf
@@ -3,6 +3,7 @@ resource "azurerm_storage_container" "container" {
   for_each = [for container in var.containers : {
     name        = container.name
     access_type = container.access_type
+    resource    = "container"
   }]
   storage_account_name  = azurerm_storage_account.storage_account.name
   name                  = each.value.name
