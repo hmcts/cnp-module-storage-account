33f1ca62c353375d7efb7fd2a2c1d7b09d8cd651
Ciarán Carragher <c.carragher@kainos.com>
2021-09-09 10:44:20 +0100

    Adds count function into mgmt policy

--- main.tf
+++ main.tf
@@ -58,7 +58,8 @@ resource "azurerm_storage_account" "storage_account" {
   )
 }
 
-resource "azurerm_storage_management_policy" "storage-account-policy" {
+rresource "azurerm_storage_management_policy" "storage-account-policy" {
+  count              = length(var.policy) > 0 ? 1 : 0
   storage_account_id = azurerm_storage_account.storage_account.id
 
   dynamic "rule" {
