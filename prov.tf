e72db77ebddb5c11becd4b10608ae52b6bbd00d0
mokainos <72137340+mokainos@users.noreply.github.com>
2022-08-04 12:22:26 +0100

    debug

---
+++ prov.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    azapi = {
+      source = "azure/azapi"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+provider "azapi" {
+}
