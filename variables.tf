a062c606db623b18a3cec81ac708add810c958ca
Himesh <himesh.patel99@hotmail.com>
2023-12-06 12:34:30 +0000

    VIH-10345 desc

--- variables.tf
+++ variables.tf
@@ -229,5 +229,5 @@ variable "pim_roles" {
 variable "retention_period" {
   type        = number
   default     = 365
-  description = "Default value for soft delete"
+  description = "Enables soft delete to be configurable to each env"
 }
