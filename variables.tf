874beec0400950d202cf243438ea3fe39da6dc47
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-09-17 12:14:52 +0100

    Add variable

--- variables.tf
+++ variables.tf
@@ -237,3 +237,7 @@ variable "retention_period" {
   default     = 365
   description = "(Optional) Specifies the number of days that the blob should be retained, between 1 and 365 days. Defaults to 365"
 }
+
+variable "cross_tenant_replication_enabled" {
+  default = false
+}
