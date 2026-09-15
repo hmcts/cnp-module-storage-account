862915a14351bfdefd42e74a70cdd0661de0acdc
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-09-15 11:08:30 +0100

    style: format storage account module

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "azurerm_storage_account" "storage_account" {
   nfsv3_enabled                    = var.enable_nfs
   public_network_access_enabled    = var.public_network_access_enabled
   cross_tenant_replication_enabled = var.cross_tenant_replication_enabled
-  shared_access_key_enabled         = var.shared_access_key_enabled
+  shared_access_key_enabled        = var.shared_access_key_enabled
 
   dynamic "immutability_policy" {
     for_each = var.immutable_enabled == true ? [1] : []
